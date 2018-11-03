fa3444c22933e01265083cce7b68b1f5ca5c2434
Mou <mohamed.m@trufla.com>
2018-11-03 05:40:46 +0200

    deploy to git

--- dist/ngx-text-highlighter/lib/pipes/safe-html.pipe.d.ts
+++
@@ -1,7 +0,0 @@
-import { PipeTransform } from '@angular/core';
-import { DomSanitizer, SafeValue } from '@angular/platform-browser';
-export declare class SafeHtmlPipe implements PipeTransform {
-    private sanitizer;
-    constructor(sanitizer: DomSanitizer);
-    transform(html: string): SafeValue;
-}
