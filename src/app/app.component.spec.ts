bdd79632b4e9a898a733040bd6c52082ea2c3037
Mou <mohamed.m@trufla.com>
2018-11-03 16:32:03 +0200

    test fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NgxTextHighlighterModule } from 'projects/ngx-text-highlighter/src/public_api';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        NgxTextHighlighterModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -22,6 +28,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngx-text-highlighter-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Ngx Text Highlighter');
   }));
 });
