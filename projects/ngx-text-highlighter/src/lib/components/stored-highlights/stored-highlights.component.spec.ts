31d4e80fe4c43c078299c2c1377c3aa104adf6c1
Mou <mohamed.m@trufla.com>
2018-11-03 18:24:43 +0200

    stored highlights test cases

--- projects/ngx-text-highlighter/src/lib/components/stored-highlights/stored-highlights.component.spec.ts
+++ projects/ngx-text-highlighter/src/lib/components/stored-highlights/stored-highlights.component.spec.ts
@@ -3,9 +3,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { StoredHighlightsComponent } from './stored-highlights.component';
 import { ColorFilterPipe } from '../../pipes/color-filter.pipe';
 
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 describe('StoredHighlightsComponent', () => {
   let component: StoredHighlightsComponent;
   let fixture: ComponentFixture<StoredHighlightsComponent>;
+  let message: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,10 +21,29 @@ describe('StoredHighlightsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StoredHighlightsComponent);
     component = fixture.componentInstance;
+    message = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show all items if no filter selected', () => {
+    const colorFilterPipe = new ColorFilterPipe();
+    const selectedFilters = [];
+    expect(component.store === colorFilterPipe.transform(component.store, selectedFilters));
+  });
+
+  it('should show appropriate message when selected filter found no matches', () => {
+    const colorFilterPipe = new ColorFilterPipe();
+    const messageText = message.query(By.css('.storage-list > span'));
+
+    if (colorFilterPipe.transform(component.store, component.selectedFilters).length < 1 && component.store.length > 1) {
+      expect(messageText.nativeElement.innerText).toBe('No items matches the selected filter');
+    }
+    if (component.store.length < 1) {
+      expect(messageText.nativeElement.innerText).toBe('You\'ve no highlighted items');
+    }
   });
 });
